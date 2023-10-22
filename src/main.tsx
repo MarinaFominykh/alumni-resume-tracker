07071542b9c4d02af8c884c3522276fed8df2fff
Maria Golovatenko <pikch4y@yandex.com>
2023-10-22 14:16:53 +0300

    feat: добавить роутинг и основные страницы

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Search from './pages/Search/Search.tsx';
+import Favourites from './pages/Favourites/Favourites.tsx';
+import Vacancies from './pages/Vacancies/Vacancies.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="/search" element={<Search />} />
+        <Route path="/favourites" element={<Favourites />} />
+        <Route path="/search" element={<Vacancies />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
+);
