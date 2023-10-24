bec9fc820a2676fd42710e0195f4cc139f74240f
Marina Fominykh <92662628+MarinaFominykh@users.noreply.github.com>
2023-10-24 18:31:20 +0300

    создать основу таблицы с выпусниками

--- src/pages/Search/Search.tsx
+++ src/pages/Search/Search.tsx
@@ -1,8 +1,11 @@
+import CustomizedTables from '../../components/Table/Table'
+import './Search.css';
 function Search() {
   return (
-    <div>
+    <div className='search'>
       {' '}
       <h1>Поиск</h1>
+      <CustomizedTables />
     </div>
   );
 }
