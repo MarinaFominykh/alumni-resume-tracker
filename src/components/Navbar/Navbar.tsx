7dd098eeb057e9296126d6186681efca463a1af0
Maria Golovatenko <pikch4y@yandex.com>
2023-10-24 14:52:49 +0300

    feat: change navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -8,26 +8,44 @@ import ListItemText from '@mui/material/ListItemText';
 import { useNavigate } from 'react-router';
 import { mainNavbarItems } from './consts/navbarLIstItems';
 import { navbarStyles } from './styles';
+import Avatar from '@mui/material/Avatar';
+import LogoutIcon from '@mui/icons-material/Logout';
+import { Box } from '@mui/material';
 
 const Navbar = () => {
   const navigate = useNavigate();
+
+  function stringAvatar(name: string) {
+    return {
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`
+    };
+  }
+
   return (
-    <div>
-      {' '}
+    <Box>
       <Drawer sx={navbarStyles.drawer} variant="permanent" anchor="left">
-        <Toolbar />
+        <Box>
+          <Avatar sx={navbarStyles.avatar} {...stringAvatar('TipTop Systems')} />
+          <Toolbar />
+          <List>
+            {mainNavbarItems.map(item => (
+              <ListItem key={item.id} disablePadding onClick={() => navigate(item.route)}>
+                <ListItemButton>
+                  <ListItemIcon sx={navbarStyles.icons}>{item.icon}</ListItemIcon>
+                  <ListItemText sx={navbarStyles.text} primary={item.label} />
+                </ListItemButton>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
         <List>
-          {mainNavbarItems.map(item => (
-            <ListItem key={item.id} disablePadding onClick={() => navigate(item.route)}>
-              <ListItemButton>
-                <ListItemIcon sx={navbarStyles.icons}>{item.icon}</ListItemIcon>
-                <ListItemText sx={navbarStyles.text} primary={item.label} />
-              </ListItemButton>
-            </ListItem>
-          ))}
+          <ListItemButton onClick={() => navigate('/sign-in')}>
+            <ListItemIcon sx={navbarStyles.icons}>{<LogoutIcon />}</ListItemIcon>
+            <ListItemText sx={navbarStyles.text} primary="Выйти" />
+          </ListItemButton>
         </List>
       </Drawer>
-    </div>
+    </Box>
   );
 };
 export default Navbar;
