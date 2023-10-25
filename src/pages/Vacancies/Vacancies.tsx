ccb73a4a5e7335b9275d80ffefb1387b1dc42e2e
“Olga <strelod@yandex.ru>
2023-10-24 22:27:38 -0700

    feat: button typography style& Vacancies update

--- src/pages/Vacancies/Vacancies.tsx
+++ src/pages/Vacancies/Vacancies.tsx
@@ -1,12 +1,27 @@
-import { Container, Typography } from "@mui/material";
+import { Button, Container, Typography, createSvgIcon } from "@mui/material";
 
 function Vacancies() {
+  const PlusIcon = createSvgIcon(
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="h-6 w-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+    </svg>,
+    'Plus',
+  );
   return (
-        <Container>
-          <Typography variant="h1" maxWidth="sm">
-            Вакансии
-          </Typography>
-        </Container>
+    <Container>
+      <Typography variant="h1">Мои вакансии</Typography>
+      <Button variant="contained">
+        <PlusIcon></PlusIcon>
+        <Typography variant="button" bgcolor={'#1D6BF3'}>Новая Вакансия</Typography>
+      </Button>
+    </Container>
   );
 }
 
