84c89995b0fd94ca4291a6ba96fb755b3f71c40b
Maria Golovatenko <pikch4y@yandex.com>
2023-10-26 17:24:52 +0300

    hotfix: изменить типы в CardList

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -1,10 +1,22 @@
 import { Stack } from '@mui/material';
 import VacancyCard from '../elements/VacancyCard/VacancyCard';
 
-function CardList({ vacancies }: any) {
+interface VacancyTypes {
+  id: string;
+  vacancy_name: string;
+  place: string;
+  city: string;
+  experience: number;
+}
+
+interface CardListTypes {
+  vacancies: [];
+}
+
+function CardList({ vacancies }: CardListTypes) {
   return (
     <Stack direction={'row'} spacing={'20px'} sx={{ overflowX: 'scroll' }}>
-      {vacancies.slice(0, 6).map((vacancy: any) => (
+      {vacancies.slice(0, 6).map((vacancy: VacancyTypes) => (
         <VacancyCard
           key={vacancy.id}
           vacancyName={vacancy.vacancy_name}
