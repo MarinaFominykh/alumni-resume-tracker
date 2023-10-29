3e2fee6ac76411986b28090aeabad073c069302f
“Olga <strelod@yandex.ru>
2023-10-28 19:16:05 -0700

    refactor: icons

--- src/components/elements/ContexMenu/ContexMenu.tsx
+++ src/components/elements/ContexMenu/ContexMenu.tsx
@@ -1,19 +1,18 @@
-import * as React from 'react';
+import * as React from "react";
 import {
   Tooltip,
   Box,
-
   Menu,
   MenuItem,
   ListItemIcon,
-
   IconButton,
   Typography,
-  } from '@mui/material';
+} from "@mui/material";
 
-import PersonAdd from '@mui/icons-material/PersonAdd';
-import Settings from '@mui/icons-material/Settings';
-import Logout from '@mui/icons-material/Logout';
+import PersonAdd from "@mui/icons-material/PersonAdd";
+import Settings from "@mui/icons-material/Settings";
+import Logout from "@mui/icons-material/Logout";
+import { ArchiveIcon, DeleteIcon, EditIcon } from "../../../consts/icons";
 
 export const ContexMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -26,16 +25,15 @@ export const ContexMenu = () => {
   };
   return (
     <React.Fragment>
-      <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-    
-        <Tooltip title='Account settings'>
+      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
+        <Tooltip title="Account settings">
           <IconButton
             onClick={handleClick}
-            size='small'
+            size="small"
             sx={{ ml: 2 }}
-            aria-controls={open ? 'account-menu' : undefined}
-            aria-haspopup='true'
-            aria-expanded={open ? 'true' : undefined}
+            aria-controls={open ? "account-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
           >
             <Typography>...</Typography>
             {/* <Avatar sx={{ width: 32, height: 32 }}>M</Avatar> */}
@@ -44,56 +42,54 @@ export const ContexMenu = () => {
       </Box>
       <Menu
         anchorEl={anchorEl}
-        id='account-menu'
+        id="account-menu"
         open={open}
         onClose={handleClose}
         onClick={handleClose}
         PaperProps={{
           elevation: 0,
           sx: {
-            overflow: 'visible',
-            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+            overflow: "visible",
+            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
             mt: 1.5,
-            '& .MuiAvatar-root': {
+            "& .MuiAvatar-root": {
               width: 32,
               height: 32,
               ml: -0.5,
               mr: 1,
             },
-            '&:before': {
+            "&:before": {
               content: '""',
-              display: 'block',
-              position: 'absolute',
+              display: "block",
+              position: "absolute",
               top: 0,
               right: 14,
               width: 10,
               height: 10,
-              bgcolor: 'background.paper',
-              transform: 'translateY(-50%) rotate(45deg)',
+              bgcolor: "background.paper",
+              transform: "translateY(-50%) rotate(45deg)",
               zIndex: 0,
             },
           },
         }}
-        transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+        transformOrigin={{ horizontal: "right", vertical: "top" }}
+        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
       >
-       
-        
         <MenuItem onClick={handleClose}>
           <ListItemIcon>
-            <PersonAdd fontSize='small' />
+            <EditIcon />
           </ListItemIcon>
           Редактировать
         </MenuItem>
         <MenuItem onClick={handleClose}>
           <ListItemIcon>
-            <Settings fontSize='small' />
+            <ArchiveIcon />
           </ListItemIcon>
           В архив
         </MenuItem>
         <MenuItem onClick={handleClose}>
           <ListItemIcon>
-            <Logout fontSize='small' />
+            <DeleteIcon />
           </ListItemIcon>
           Удалить
         </MenuItem>
