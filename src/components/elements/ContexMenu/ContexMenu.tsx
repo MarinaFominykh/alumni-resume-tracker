8de3f17fb9ec6ca89927707d2b9e735b0294b8e7
Maria Golovatenko <pikch4y@yandex.com>
2023-10-29 19:49:41 +0300

    feat: изменить стили у выпадающего меню

--- src/components/elements/ContexMenu/ContexMenu.tsx
+++ src/components/elements/ContexMenu/ContexMenu.tsx
@@ -1,15 +1,9 @@
-import * as React from "react";
-import {
-  Tooltip,
-  Box,
-  Menu,
-  MenuItem,
-  ListItemIcon,
-  IconButton,
-  Typography,
-} from "@mui/material";
-
-import { ArchiveIcon, DeleteIcon, EditIcon } from "../../../consts/icons";
+import * as React from 'react';
+import IconButton from '@mui/material/IconButton';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+import { ArchiveIcon, DeleteIcon, EditIcon } from '../../../consts/icons';
 
 export const ContexMenu = () => {
   const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
@@ -20,74 +14,45 @@ export const ContexMenu = () => {
   const handleClose = () => {
     setAnchorEl(null);
   };
+
   return (
     <React.Fragment>
-      <Box sx={{ display: "flex", alignItems: "center", textAlign: "center" }}>
-        <Tooltip title="Account settings">
-          <IconButton
-            onClick={handleClick}
-            size="small"
-            sx={{ ml: 2 }}
-            aria-controls={open ? "account-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-          >
-            <Typography>...</Typography>
-            {/* <Avatar sx={{ width: 32, height: 32 }}>M</Avatar>  */}
-          </IconButton>
-        </Tooltip>
-      </Box>
+      <IconButton
+        aria-label="more"
+        id="long-button"
+        aria-controls={open ? 'long-menu' : undefined}
+        aria-expanded={open ? 'true' : undefined}
+        aria-haspopup="true"
+        onClick={handleClick}
+      >
+        <MoreHorizIcon color="primary" />
+      </IconButton>
+
       <Menu
+        id="long-menu"
+        MenuListProps={{
+          'aria-labelledby': 'long-button'
+        }}
         anchorEl={anchorEl}
-        id="account-menu"
         open={open}
         onClose={handleClose}
-        onClick={handleClose}
         PaperProps={{
-          elevation: 0,
-          sx: {
-            overflow: "visible",
-            filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
-            mt: 1.5,
-            "& .MuiAvatar-root": {
-              width: 32,
-              height: 32,
-              ml: -0.5,
-              mr: 1,
-            },
-            "&:before": {
-              content: '""',
-              display: "block",
-              position: "absolute",
-              top: 0,
-              right: 14,
-              width: 10,
-              height: 10,
-              bgcolor: "background.paper",
-              transform: "translateY(-50%) rotate(45deg)",
-              zIndex: 0,
-            },
-          },
+          style: {
+            width: '20ch',
+            padding: '12px'
+          }
         }}
-        transformOrigin={{ horizontal: "right", vertical: "top" }}
-        anchorOrigin={{ horizontal: "right", vertical: "bottom" }}
+        anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
       >
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <EditIcon />
-          </ListItemIcon>
+        <MenuItem onClick={handleClose} disableRipple>
+          <EditIcon sx={{ mr: '8px' }} />
           Редактировать
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <ArchiveIcon />
-          </ListItemIcon>
-          В архив
+        <MenuItem onClick={handleClose} disableRipple>
+          <ArchiveIcon sx={{ mr: '8px' }} />В архив
         </MenuItem>
-        <MenuItem onClick={handleClose}>
-          <ListItemIcon>
-            <DeleteIcon />
-          </ListItemIcon>
+        <MenuItem onClick={handleClose} disableRipple>
+          <DeleteIcon sx={{ mr: '8px' }} />
           Удалить
         </MenuItem>
       </Menu>
